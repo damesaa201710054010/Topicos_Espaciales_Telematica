5f704e3cafc830c71de9a326ba3051de8428fa1a
damesaa201710054010 <damesaa@eafit.edu.co>
2020-02-02 19:57:52 -0500

    deleted spam structure

--- server/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const URI = process.env.MONGOOSE_URI
-    ? process.env.MONGOOSE_URI
-    : 'mongodb://localhost/telematicsApp';
-
-mongoose.connect(URI)
-        .then(db => console.log('Data base connected'))
-        .catch(err => console.log(err));
--- server/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose;
-
-const users = new Schema({
-    user: {type: String, required: true},
-    password: {type: String, required: true}
-});
-
-module.exports = mongoose.model('user', users);
